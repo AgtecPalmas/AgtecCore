66a039b4a118d1ec1894efbbae57a4d7a4f831cb
ThigSchuch <thigschuch@gmail.com>
2023-08-21 17:29:29 -0300

    feat: Construa apenas o projeto FastAPI inicial sem apps
    - .env populada com a Secret do projeto Django
    - Criado ProjetoBuild para a inicialização do projeto FastAPI
    - Utilize "--base" para iniciar o projeto sem apps

--- {{cookiecutter.project_slug}}/core/management/commands/fastapi_managers/__init__.py
+++ {{cookiecutter.project_slug}}/core/management/commands/fastapi_managers/__init__.py
@@ -2,6 +2,7 @@ from .build_routers import RoutersBuild
 from .build_use_cases import UseCasesBuild
 from .build_docker import DockerBuild
 from .build_models import ModelsBuild
+from .build_projeto import ProjetoBuild
 from .build_schemas import SchemasBuild
 
 __all__ = [
@@ -9,5 +10,6 @@ __all__ = [
     "UseCasesBuild",
     "DockerBuild",
     "ModelsBuild",
+    "ProjetoBuild",
     "SchemasBuild",
 ]
