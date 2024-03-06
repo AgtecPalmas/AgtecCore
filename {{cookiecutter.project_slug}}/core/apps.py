8867dfbd54e9f6b5bf275be8c586dcbde47cfda6
ThigSchuch <thigschuch@gmail.com>
2024-03-06 17:31:02 -0300

    build: Bump version

--- {{cookiecutter.project_slug}}/core/apps.py
+++ {{cookiecutter.project_slug}}/core/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class BaseConfig(AppConfig):
     name = "core"
     icon = "fas fa-heart"
-    version = "4.2.1"
-    codename = "Buriti"
+    version = "4.3.0"
+    codename = "Cajuí"
