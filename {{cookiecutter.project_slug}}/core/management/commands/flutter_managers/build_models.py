b5cb57133d6c460263af837eadec7d1dd68ad3c5
ThigSchuch <thigschuch@gmail.com>
2024-03-07 15:19:52 -0300

    fix: Correções Sourcery Sonar e CodeQL

--- {{cookiecutter.project_slug}}/core/management/commands/flutter_managers/build_models.py
+++ {{cookiecutter.project_slug}}/core/management/commands/flutter_managers/build_models.py
@@ -28,11 +28,7 @@ class ModelsBuilder:
         self._content_constructor = ""
         self._ignored_fields = ["enabled", "deleted", "createdOn", "updatedOn"]
         self._model_path_file = Path(
-            "{}/lib/apps/{}/{}/model.dart".format(
-                self._path_flutter,
-                self._app_name_lower,
-                self._model_name_lower,
-            )
+            f"{self._path_flutter}/lib/apps/{self._app_name_lower}/{self._model_name_lower}/model.dart"
         )
         self._snippet_model = Utils.get_snippet(
             str(Path(f"{self.snippet_dir}/model.txt"))
@@ -44,30 +40,40 @@ class ModelsBuilder:
                 Utils.show_error(f"File is locked: {self._model_path_file}")
                 return
             for field in iter(self.app.model._meta.fields):
-                _app, _model, _name = str(field).split(".")
+                _, _, _name = str(field).split(".")
                 _name_dart = convert_to_camel_case(_name)
+
                 if _name_dart in [f"id{self._model_name_lower}", "id"]:
                     continue
+
                 _field_type = (
                     str(str(type(field)).split(".")[-1:])
                     .replace('["', "")
                     .replace("'>\"]", "")
                 )
+
                 _atribute = FLUTTER_TYPES[DJANGO_TYPES.index(_field_type)]
+
                 if _name_dart not in ["enabled", "deleted", "createdOn", "updatedOn"]:
                     self._content_atributes += f"{_atribute} {_name_dart};\n  "
+
                 if _name_dart not in DJANGO_USER_FIELDS:
                     self._content_string_return += (
                         f"{_name_dart.upper()}: ${_name_dart}\\n"
                     )
+
                 if _name_dart not in self._ignored_fields:
                     default_value = None
+
                     if str(_atribute) == "int":
                         default_value = 0
+
                     if str(_atribute) == "double":
                         default_value = 0.0
+
                     if str(_atribute) == "bool":
                         default_value = "true"
+
                     if str(_atribute) == "String":
                         default_value = "''"
 
@@ -75,78 +81,39 @@ class ModelsBuilder:
                         self._content_constructor += f"DateTime? {_name_dart},"
 
                     if default_value is not None:
-                        self._content_constructor += "this.{} = {},\n".format(
-                            _name_dart, default_value
+                        self._content_constructor += (
+                            f"this.{_name_dart} = {default_value},\n"
                         )
 
-                if _name_dart not in self._ignored_fields:
                     if str(_atribute) == "DateTime?":
-                        self._content_from_json += "{}: map.containsKey('{}')? Util.convertDate(map['{}']): null, \n".format(
-                            _name_dart, _name, _name
-                        )
+                        self._content_from_json += f"{_name_dart}: map.containsKey('{_name}')? Util.convertDate(map['{_name}']): null, \n"
+
                     elif str(_atribute) == "double":
                         self._content_from_json += (
-                            "{1}: map['{2}'] ?? 0.0,\n{0}".format(
-                                " " * 8, _name_dart, _name
-                            )
+                            f"{_name_dart}: map['{_name}'] or 0.0,\n{' ' * 8}"
                         )
+
                     elif str(_atribute) == "bool":
                         self._content_from_json += (
-                            "{1}: map['{2}'] ?? false,\n{0}".format(
-                                " " * 8, _name_dart, _name
-                            )
+                            f"{_name_dart}: map['{_name}'] or False,\n{' ' * 8}"
                         )
-                    else:
-                        if _name_dart.startswith("fk"):
-                            self._content_from_json += (
-                                "{1}: map['{2}'] ?? '',\n{0}".format(
-                                    " " * 8, _name_dart, _name
-                                )
-                            )
-                        else:
-                            self._content_from_json += (
-                                "{1}: map['{2}'] ?? '',\n{0}".format(
-                                    " " * 8, _name_dart, _name
-                                )
-                            )
 
-                if str(_field_type) == "DateTimeField":
-                    self._content_to_map += "'{}': Util.stringDateTimeSplit".format(
-                        _name
-                    )
-                    self._content_to_map += "({}, returnType: 'dt'),\n{}".format(
-                        _name_dart, " " * 8
-                    )
+                    else:
+                        self._content_from_json += (
+                            f"{_name_dart}: map['{_name}'] or '',\n{' ' * 8}"
+                        )
+
+                if _field_type in {"DateTimeField", "DateField", "TimeField"}:
+                    return_types = {
+                        "DateTimeField": "dt",
+                        "DateField": "d",
+                        "TimeField": "t",
+                    }
+                    self._content_to_map += f"'{_name}': Util.stringDateTimeSplit"
+                    self._content_to_map += f"""({_name_dart}, returnType: '{return_types[_field_type]}'),\n{" " * 8}"""
                     continue
-                if str(_field_type) == "DateField":
-                    self._content_to_map += "'{}': Util.stringDateTimeSplit".format(
-                        _name
-                    )
-                    self._content_to_map += "({}, returnType: 'd'),\n{}".format(
-                        _name_dart, " " * 8
-                    )
-                    continue
-                if str(_field_type) == "TimeField":
-                    self._content_to_map += "'{}': Util.stringDateTimeSplit".format(
-                        _name
-                    )
-                    self._content_to_map += "({}, returnType: 't'),\n{}".format(
-                        _name_dart, " " * 8
-                    )
-                    continue
-                if str(_field_type) in ["FloatField", "DecimalField"]:
-                    self._content_to_map += "'{0}': {1},\n{2}".format(
-                        _name, _name_dart, " " * 8
-                    )
-                    continue
-                if str(_atribute) == "bool":
-                    self._content_to_map += "'{0}': {1},\n{2}".format(
-                        _name, _name_dart, " " * 8
-                    )
-                    continue
-                self._content_to_map += "'{0}': {1},\n{2}".format(
-                    _name, _name_dart, " " * 8
-                )
+
+                self._content_to_map += f"'{_name}': {_name_dart},\n{' ' * 8}"
 
             if not Utils.check_file(self._model_path_file):
                 os.makedirs(self._model_path_file)
