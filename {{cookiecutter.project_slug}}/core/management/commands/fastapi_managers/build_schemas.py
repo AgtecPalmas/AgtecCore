214e73e9c8078965ddbb408864bec745eb4f2645
Guilherme de Carvalho Carneiro <guilherme.carvalho.carneiro@gmail.com>
2024-10-25 11:23:09 -0300

    refactor: Atualizando os managers
    Atualizando os managers do build do FastAPI

--- {{cookiecutter.project_slug}}/core/management/commands/fastapi_managers/build_schemas.py
+++ {{cookiecutter.project_slug}}/core/management/commands/fastapi_managers/build_schemas.py
@@ -18,7 +18,7 @@ class SchemasBuild:
         self.app_instance = self.command.app_instance
         self._fields_types = FIELDS_TYPES
 
-    def __get_field_django_type(self, field):
+    def __get_field_django_type(self, field): # noqa
         return (
             str(str(type(field)).split(".")[-1:]).replace('["', "").replace("'>\"]", "")
         )
@@ -26,12 +26,12 @@ class SchemasBuild:
     def __check_django_type_in_fields_types(self, field):
         return field.__class__.__name__ in self._fields_types.keys()
 
-    def __add_attr_null(self, field, attribute):
+    def __add_attr_null(self, field, attribute): # noqa
         if getattr(field, "null", None):
             attribute = f"Optional[{attribute}] = None"
         return attribute
 
-    def __add_attr_default(self, field, attribute):
+    def __add_attr_default(self, field, attribute): # noqa
         if field.get_default() is not None and field.get_default() != "":
             field_str = field.get_default()
 
@@ -59,15 +59,19 @@ class SchemasBuild:
 
     def build(self):
         try:
-            if Utils.check_content(self.path_schema, f"class {self.model}"):
-                Utils.show_message("[cyan]Schemas[/] já existem")
+            if Utils.check_content(self.path_schema, f"class {self.model}"): # noqa
+                Utils.show_message("[cyan]Schemas[/] já existem") # noqa
                 return
 
-            content = Utils.get_snippet(str(Path(f"{self.snippet_dir}/schema.txt")))
-            content = content.replace("$ModelClass$", self.model)
             model = self.app_instance.get_model(self.model)
-            fields = model._meta.fields
+            class_is_inherited = model.__bases__[0].__name__ != "Base"
+            content = Utils.get_snippet(str(Path(f"{self.snippet_dir}/schema.txt")))
+            if class_is_inherited is True:
+                content = Utils.get_snippet(str(Path(f"{self.snippet_dir}/schema_inherited.txt")))
+            content = content.replace("$ModelClass$", self.model)
+            fields = model._meta.fields # noqa
             result = ""
+            fields_db = ""
 
             for field in iter(fields):
                 item = {
@@ -98,24 +102,42 @@ class SchemasBuild:
                     attribute = self.__add_attr_default(field, attribute)
 
                     if item.get("django_type") in ["ForeignKey", "OneToOneField"]:
+                        # Checando se a classe herda de outro models e é o django_user
+                        # para não renderizar o campo
+                        if class_is_inherited is True and field_name == 'django_user':
+                            continue
+
                         if str(field_name).endswith("_id"):
-                            result += f"\t {field_name}: Optional[UUID]\n"
+                            result += f"\t{field_name}: Optional[UUID]\n"
                             continue
 
                         field_name = field.get_attname_column()[1]
-                    result += f"\t {field_name}: {attribute}\n"
+
+                    # Checando se a classe herda de outro models
+                    # para não renderizar os atributos do models Pai no Schema
+                    if class_is_inherited is True:
+                        _itens_classe = list(model.__dict__.keys())
+                        if field_name in field_name in _itens_classe:
+                            result += f"\t{field_name}: {attribute}\n"
+                            # Filtrando apenas os campos do models mesmo, removendo o
+                            # que termina com _ptr_id
+                            if str(field_name).endswith('_ptr_id') is False:
+                                fields_db += f"    {field_name}: {attribute}\n"
+                    else:
+                        result += f"\t{field_name}: {attribute}\n"
 
             # Many to Many
-            for related_field in iter(model._meta.many_to_many):
+            for related_field in iter(model._meta.many_to_many): # noqa
                 attribute = "Set"
                 attribute = self.__add_attr_null(related_field, attribute)
                 attribute = self.__add_attr_default(related_field, attribute)
-                result += f"\t {related_field.name}: {attribute}\n"
+                result += f"\t{related_field.name}: {attribute}\n"
 
             content = content.replace("$auth_import$", "")
             content = content.replace("$fields$", result)
+            content = content.replace("$fields_db$", fields_db)
 
-            if Utils.check_file(self.path_schema) is False:
+            if Utils.check_file(self.path_schema) is False: # noqa
                 with open(self.path_schema, "w") as arquivo:
                     arquivo.write(content)
 
@@ -124,10 +146,10 @@ class SchemasBuild:
                     schema.write("\n")
                     schema.write(content)
 
-            PythonFormatter(self.path_schema).format()
-            Utils.show_message("[cyan]Schemas[/] criados com sucesso")
+            PythonFormatter(self.path_schema).format() # noqa
+            Utils.show_message("[cyan]Schemas[/] criados com sucesso") # noqa
 
         except Exception as e:
             Utils.show_message(
-                f"Erro ao criar o Schema do model {self.model}. Erro: {e}"
+                f"Erro ao criar o Schema do model {self.model}. Erro: {e}" # noqa
             )
