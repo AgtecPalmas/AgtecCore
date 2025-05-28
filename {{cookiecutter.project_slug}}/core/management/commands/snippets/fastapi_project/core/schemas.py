3011d128939e5e424a88acc1b5b35d6940109bd5
Guilherme de Carvalho Carneiro <guilherme.carvalho.carneiro@gmail.com>
2025-05-28 08:51:07 -0300

    refactor: :recycle: Ajustando o snippet do paginate

--- {{cookiecutter.project_slug}}/core/management/commands/snippets/fastapi_project/core/schemas.py
+++ {{cookiecutter.project_slug}}/core/management/commands/snippets/fastapi_project/core/schemas.py
@@ -1,10 +1,8 @@
-from typing import List, Optional, Union
-
 from pydantic import BaseModel
 
 
 class PaginationBase(BaseModel):
     count: int = 0
-    next: Union[str, None] = None
-    previous: Union[str, None] = None
-    results: Optional[List[dict]] = None
+    next: str | None = None
+    previous: str | None = None
+    results: list[dict] | None = None
